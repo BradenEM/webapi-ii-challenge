066d783b26e190e9082cee61380b7265bf94317e
Braden Mason <bradenemason@gmail.com>
2019-07-28 18:06:22 -0500

    upating posts works

--- postsRouter.js
+++ postsRouter.js
@@ -65,4 +65,14 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await db.update(req.params.id, req.body);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
