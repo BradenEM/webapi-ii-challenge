0b2f552377c7de06457d1bd4984abaf8f4a78d3e
Braden Mason <bradenemason@gmail.com>
2019-07-28 19:34:06 -0500

    Most of specifications done besides for a few 404 responses

--- postsRouter.js
+++ postsRouter.js
@@ -8,48 +8,91 @@ router.get("/", async (req, res) => {
 
     res.status(200).json(posts);
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(500)
+      .json({ error: "The posts information could not be retrieved." });
   }
 });
+
+// !404 not working here VVV
 
 router.get("/:id", async (req, res) => {
   try {
     const post = await db.findById(req.params.id);
 
-    res.status(200).json(post);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
   }
 });
+
+// !404 not working here VVV
 
 router.get("/:id/comments", async (req, res) => {
   try {
     const comments = await db.findPostComments(req.params.id);
 
-    res.status(200).json(comments);
+    if (comments) {
+      res.status(200).json(comments);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(500)
+      .json({ error: "The comments information could not be retrieved." });
   }
 });
 
 router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide a title and contents for the post."
+    });
+  }
   try {
     const body = await db.insert(req.body);
-    res.status(200).json(body);
+    res.status(201).json(body);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
   }
 });
 
 router.post("/:id/comments", async (req, res) => {
   const { text } = req.body;
   const post_id = req.params.id;
+
+  if (!text) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+  }
   try {
     const comment = await db.insertComment({ text, post_id });
 
-    res.status(200).json(comment);
+    res.status(201).json(comment);
   } catch (error) {
-    res.status(500).json(error);
+    if (error.errno === 19) {
+      res
+        .status(400)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+    res.status(500).json({
+      error: "There was an error while saving the comment to the database"
+    });
   }
 });
 
@@ -59,17 +102,26 @@ router.delete("/:id", async (req, res) => {
 
     res.status(200).json(post);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({ error: "The post could not be removed" });
   }
 });
 
 router.put("/:id", async (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
   try {
     const post = await db.update(req.params.id, req.body);
 
     res.status(200).json(post);
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(500)
+      .json({ error: "The post information could not be modified." });
   }
 });
 
